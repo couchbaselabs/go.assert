e220b067da3ea8238e6c3a91b5045b7d9e505fea
Steven Degutis <steven.degutis@gmail.com>
2012-02-20 13:05:59 -0600

    refactor!

--- assert.go
+++ assert.go
@@ -9,32 +9,31 @@ type TestDriver interface {
   Errorf(format string, args ...interface{})
 }
 
+func auxiliaryInfo() (filename string, line int, code string) {
+  _, file, line, _ := runtime.Caller(2)
+  buf, _ := ioutil.ReadFile(file)
+  filename = path.Base(file)
+  code = strings.TrimSpace(strings.Split(string(buf), "\n")[line-1])
+  return
+}
+
 func Equals(t TestDriver, got, expected interface{}) {
   if got != expected {
-    _, file, line, _ := runtime.Caller(1)
-    buf, _ := ioutil.ReadFile(file)
-    filename := path.Base(file)
-    code := strings.TrimSpace(strings.Split(string(buf), "\n")[line-1])
+    filename, line, code := auxiliaryInfo()
     t.Errorf("|||||| %v:%d ----- %s ----- expected: %#v ----- got: %#v", filename, line, code, expected, got)
   }
 }
 
 func True(t TestDriver, got bool) {
   if got != true {
-    _, file, line, _ := runtime.Caller(1)
-    buf, _ := ioutil.ReadFile(file)
-    filename := path.Base(file)
-    code := strings.TrimSpace(strings.Split(string(buf), "\n")[line-1])
+    filename, line, code := auxiliaryInfo()
     t.Errorf("|||||| %v:%d ----- %s", filename, line, code)
   }
 }
 
 func False(t TestDriver, got bool) {
   if got != false {
-    _, file, line, _ := runtime.Caller(1)
-    buf, _ := ioutil.ReadFile(file)
-    filename := path.Base(file)
-    code := strings.TrimSpace(strings.Split(string(buf), "\n")[line-1])
+    filename, line, code := auxiliaryInfo()
     t.Errorf("|||||| %v:%d ----- %s", filename, line, code)
   }
 }
