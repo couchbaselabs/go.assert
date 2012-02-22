7a5108490bcb966c169912a259352fc003613d51
Steven Degutis <steven.degutis@gmail.com>
2012-02-22 07:45:32 -0600

    refactor, and purple

--- assert.go
+++ assert.go
@@ -12,44 +12,44 @@ type TestDriver interface {
 }
 
 func auxiliaryInfo() (filename string, line int, code string) {
-  _, file, line, _ := runtime.Caller(2)
+  _, file, line, _ := runtime.Caller(3)
   buf, _ := ioutil.ReadFile(file)
   filename = path.Base(file)
   code = strings.TrimSpace(strings.Split(string(buf), "\n")[line-1])
   return
 }
 
+func codeErrorString() string {
+  filename, line, code := auxiliaryInfo()
+  return col.Red.Fmt("\n%v:%d\n\n%s", filename, line, code)
+}
+
 func Equals(t TestDriver, got, expected interface{}) {
   if got != expected {
-    filename, line, code := auxiliaryInfo()
-    t.Errorf(col.Red.Fmt("\n%v:%d\n\n%s\n\n\texpected: %#v\n\t     got: %#v", filename, line, code, expected, got))
+    t.Errorf(codeErrorString() + col.Purple.Fmt("\n\n\texpected: %#v\n\t     got: %#v", expected, got))
   }
 }
 
 func DeepEquals(t TestDriver, got, expected interface{}) {
   if !reflect.DeepEqual(got, expected) {
-    filename, line, code := auxiliaryInfo()
-    t.Errorf(col.Red.Fmt("\n%v:%d\n\n%s\n\n\texpected: %#v\n\t     got: %#v", filename, line, code, expected, got))
+    t.Errorf(codeErrorString() + col.Purple.Fmt("\n\n\texpected: %#v\n\t     got: %#v", expected, got))
   }
 }
 
 func NotEquals(t TestDriver, got, expected interface{}) {
   if got == expected {
-    filename, line, code := auxiliaryInfo()
-    t.Errorf(col.Red.Fmt("\n%v:%d\n\n%s\n\n\tunexpectedly got: %#v", filename, line, code, got))
+    t.Errorf(codeErrorString() + col.Purple.Fmt("\n\n\tunexpectedly got: %#v", got))
   }
 }
 
 func True(t TestDriver, got bool) {
   if got != true {
-    filename, line, code := auxiliaryInfo()
-    t.Errorf(col.Red.Fmt("\n%v:%d\n\n%s", filename, line, code))
+    t.Errorf(codeErrorString())
   }
 }
 
 func False(t TestDriver, got bool) {
   if got != false {
-    filename, line, code := auxiliaryInfo()
-    t.Errorf(col.Red.Fmt("\n%v:%d\n\n%s", filename, line, code))
+    t.Errorf(codeErrorString())
   }
 }
